8cd8db054db15295eea08931d9c8fabda0157b87
Adam Gonzalez <adam.gonz@outlook.com>
2023-04-02 20:25:07 -0700

    needed a head to make website have icon when loading and a better title

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+'use client'
+export default function Head() {
+  return (
+    <>
+      <title>Adam Gonzalez</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="Rbi Softball" content="Softball Stats" />
+      <link rel="icon" href="/softball.ico" />
+    </>
+  )
+}
